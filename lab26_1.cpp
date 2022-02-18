e74633cae78ac3bbdf8942957092974e0cfc7de4
Naihtton <whitediary1@outlook.com>
2022-02-18 16:56:02 +0700

    Finished lab26_1.cpp

--- lab26_1.cpp
+++ lab26_1.cpp
@@ -64,3 +64,49 @@ void List::append(int d){
 }
 
 //Write List::remove() here
+void List::remove(int idx){
+
+	size-- ;
+	Node *current = root ;
+	if(idx == 0){
+		root = current->next ;
+		delete current ;
+		return ;
+	}
+	for(int i = 0;i < idx-1 ;i++){
+		current = current->next ;
+	}
+	Node *temp = current->next ;
+	current->next = temp->next ;
+
+	delete temp ;
+
+
+}
+
+int main(){
+	List myList = {0,0};
+myList.append(5);
+myList.append(7);
+myList.append(11);
+myList.append(4);
+myList.append(12);
+myList.append(45);
+
+myList.insert(29,2);
+myList.insert(33,3);
+myList.insert(77,5);
+myList.insert(69,0);
+myList.show();
+cout << "\n"; 
+
+myList.remove(2);
+myList.show();
+cout << "\n"; 
+myList.remove(4);
+myList.show();
+cout << "\n";
+myList.remove(0);
+myList.show();
+cout << "\n";
+}
